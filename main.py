37df2b3b07b8ddeaef4164eb25da35ba96fcf0ed
Shruti4105 <shruti.p.shinde@gmail.com>
2026-02-10 09:00:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,5 @@ import multiplication as m
 print("Addition Result : " , a.add(5,6))
 print("Subtraction Result : " , s.sub(10,6))
 print("Multiplication Result : " , m.multi(7,6))
+
+# Call Division Function
