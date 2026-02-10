9004657dfd4c5cf601e2297bdb833bbf71b1a675
shruti <shruti.p.shinde@gmail.com>
2026-02-10 08:43:07 +0530

    Merge All The Files.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 #Calculator 
+
+import addition as a
+import subtraction as s
+import multiplication as m
+
+print("Addition Result : " , a.add(5,6))
+print("Subtraction Result : " , s.sub(10,6))
+print("Multiplication Result : " , m.multi(7,6))
