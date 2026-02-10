4fb3887fa03e4478630e514f29dc8783addb8810
shruti <shruti.p.shinde@gmail.com>
2026-02-10 09:18:07 +0530

    Merge all the files.

--- main.py
+++ main.py
@@ -3,9 +3,11 @@
 import addition as a
 import subtraction as s
 import multiplication as m
+import division as d
 
 print("Addition Result : " , a.add(5,6))
 print("Subtraction Result : " , s.sub(10,6))
 print("Multiplication Result : " , m.multi(7,6))
+print("Division Result : " , d.divis(7,6))
 
 # Call Division Function
